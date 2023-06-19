18ef94db8b7620aded772f1ec92138f762e833d2
Marcin Bojara <marcin_5@mail.ru>
2023-06-19 23:30:47 +0200

    add account model for users

--- users/models.py
+++ users/models.py
@@ -1,3 +1,36 @@
+from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
+from django.contrib.auth.models import PermissionsMixin
 from django.db import models
 
-# Create your models here.
+
+class AccountManager(BaseUserManager):
+    def create_user(self, email, first_name, last_name, password=None):
+        user = self.model(email=self.normalize_email(email), first_name=first_name, last_name=last_name)
+        user.set_password(password)
+        user.is_active = True
+        user.save(using=self.db)
+        return user
+
+    def create_superuser(self, email, first_name, last_name, password=None):
+        user = self.create_user(email=email, first_name=first_name, last_name=last_name, password=password)
+        user.is_active = True
+        user.is_superuser = True
+        user.save(using=self.db)
+        return user
+
+
+class Account(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(max_length=60, unique=True)
+    first_name = models.CharField(max_length=15)
+    last_name = models.CharField(max_length=30)
+
+    is_active = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.email
+
+    objects = AccountManager()
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["first_name", "last_name"]
