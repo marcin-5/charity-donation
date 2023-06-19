3029aa8caafd4be3003e78c3b403b5369db0593f
Marcin Bojara <marcin_5@mail.ru>
2023-06-19 19:50:39 +0200

    fix getting environment variables

--- config/settings.py
+++ config/settings.py
@@ -80,11 +80,11 @@ WSGI_APPLICATION = "config.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": environ("POSTGRES_DB"),
-        "USER": environ("POSTGRES_USER"),
-        "PASSWORD": environ("POSTGRES_PASSWORD"),
-        "HOST": environ("POSTGRES_HOST"),
-        "PORT": environ("POSTGRES_PORT"),
+        "NAME": environ.get("POSTGRES_DB"),
+        "USER": environ.get("POSTGRES_USER"),
+        "PASSWORD": environ.get("POSTGRES_PASSWORD"),
+        "HOST": environ.get("POSTGRES_HOST"),
+        "PORT": environ.get("POSTGRES_PORT"),
     },
 }
 
