ea62903b48ff8b48dbbbed3708ce57f6596da7ee
Marcin Bojara <marcin_5@mail.ru>
2023-06-21 18:24:08 +0200

    get statistics from db

--- home/views.py
+++ home/views.py
@@ -1,10 +1,17 @@
+from django.db.models import Sum
 from django.shortcuts import render
 from django.views import View
+
+from home.models import Donation
 
 
 class LandingPageView(View):
     def get(self, request):
-        return render(request, "home/index.html")
+        ctx = {
+            "sum_": Donation.objects.aggregate(Sum("quantity"))["quantity__sum"] or 0,
+            "len_": len(Donation.objects.values("institution").distinct()),
+        }
+        return render(request, "home/index.html", ctx)
 
 
 class AddDonationView(View):
