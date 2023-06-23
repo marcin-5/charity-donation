a0853b31cfe32ae6cac2e29f434ef0320dc6c087
Marcin Bojara <marcin_5@mail.ru>
2023-06-23 12:10:05 +0200

    dynamic loading of institution data

--- home/views.py
+++ home/views.py
@@ -2,14 +2,17 @@ from django.db.models import Sum
 from django.shortcuts import render
 from django.views import View
 
-from home.models import Donation
+from home.models import Donation, Institution
 
 
 class LandingPageView(View):
     def get(self, request):
         ctx = {
             "sum_": Donation.objects.aggregate(Sum("quantity"))["quantity__sum"] or 0,
-            "len_": len(Donation.objects.values("institution").distinct()),
+            "len_": Donation.objects.values("institution").distinct().count(),
+            "foundations": Institution.objects.filter(type="F"),
+            "organizations": Institution.objects.filter(type="OP"),
+            "collections": Institution.objects.filter(type="ZL"),
         }
         return render(request, "home/index.html", ctx)
 
